dfcdb7e5dd139a7d8a77fe13083b3f12f6b01408
Evgeniy <evgnec@gmail.com>
2023-03-13 23:11:29 +0200

    hw06

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,8 @@
+
+// Example1
+// ==============================================
 const technologies = ["HTML1", "CSS1", "JavaScript1", "React1", "Node1"];
-const listEL = document.createElement("ul");
-
-
+// const listEL = document.createElement("ul");
 // for (const technology of technologies) {
 //     const liEl = document.createElement("li");
 //     liEl.textContent = technology;
@@ -9,11 +10,24 @@ const listEL = document.createElement("ul");
 // }
 // document.body.appendChild(listEL);
 
-for (const technology of technologies) { 
-    const liEl = document.createElement("li");
-    const text = document.createTextNode(technology);
-    liEl.appendChild(text);   
-    listEL.appendChild(liEl);
-}
-document.body.appendChild(listEL);
+// for (const technology of technologies) {
+//     const liEl = document.createElement("li");
+//     const text = document.createTextNode(technology);
+//     liEl.appendChild(text);
+//     listEL.appendChild(liEl);
+// }
+// document.body.appendChild(listEL);
+
+// =================== map ===========================
+// const markup = `<ul>${technologies.map(technology => `<li>${technology}<li>`).join("")}</ul>`
+// document.body.insertAdjacentHTML("afterbegin", markup);
+
+ // =================== reduce ===========================
+// const markup = `<ul>${technologies.reduce(
+//     (acc, technology) => (acc += `<li>${technology}</li>`), "")
+//     }</ul>`
+
+//     document.body.insertAdjacentHTML("afterbegin", markup);
+
+
 
