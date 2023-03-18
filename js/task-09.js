1f19d6961c45e6f2a86cd9a7e0428674f95aaf80
Evgeniy <evgnec@gmail.com>
2023-03-18 16:15:38 +0200

    add 8-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,21 @@
+
+const changeEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const textEl = document.querySelector('.color');
+
+changeEl.addEventListener('click', changeElClick);
+
+function changeElClick(event) {
+  let backgroundColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = backgroundColor;
+  textEl.textContent = backgroundColor;
+
+};
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
