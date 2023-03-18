fb45f8077095636de0a2edea0b8fb344aae3722b
Evgeniy <evgnec@gmail.com>
2023-03-18 10:55:13 +0200

    add 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,39 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(event) {
+//     event.preventDefault();
+//     if (event.currentTarget.elements.email.value === "")
+//         alert("Input login");
+//     else if (event.currentTarget.elements.password.value === "")
+//         alert("Input passwords");
+//     else {
+//         elements.email = event.currentTarget.elements.email.value;
+//         elements.password = event.currentTarget.elements.password.value;
+//     }
+// };
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    if (event.currentTarget.elements.email.value === "")
+        alert("Input login");
+    else if (event.currentTarget.elements.password.value === "")
+        alert("Input passwords");
+    else {
+        const formData = new FormData(event.currentTarget);   
+         formData.forEach((x, y) => { 
+     console.log("name: ", y);
+     console.log("value: ", x);        
+         }) 
+        resetForm();
+    }
+};
+
+function resetForm() { 
+form.reset(); 
+}
+
 
